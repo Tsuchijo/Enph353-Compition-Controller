d0c519e82afb174e928ea62f0025986b52ff3141
fizzer <TsuchitoriJoel@gmail.com>
2023-03-30 04:53:19 -0700

    added line detection based state-machine

--- src/move_robot.py
+++ src/move_robot.py
@@ -11,46 +11,137 @@ from std_msgs.msg import String
 #create cv2 bridge 
 bridge = CvBridge()
 
-# Display image in a window called "camera_image.jpeg"
-cv2.namedWindow('camera_image.jpeg', cv2.WINDOW_NORMAL)
+#create state machine
+state = 0
+vert_angle = 0
+horizontal_line = False
 
-#create sliders to test values
-cv2.createTrackbar('Saturation', 'camera_image.jpeg', 0, 100, lambda x: None)
-cv2.createTrackbar('Masking', 'camera_image.jpeg', 0, 255, lambda x: None)
+# create a state machine which follows the vert angle as long as it does not see a horizontal line, then it will stop at the line
+def state_machine():
+    global state
+    global vert_angle
+    global horizontal_line
+    #start at state 0 and start timer then move onto state 1
+    if state == 0:
+        start_timer()
+        state = 1
+    #move forward until it sees a horizontal line
+    if state == 1:
+        if horizontal_line == False:
+            move_robot(0.1, vert_angle/100)
+        else:
+            state = 2
+    #stop at the horizontal line and stop timer
+    if state == 2:
+        print("stop")
+        move_robot(-0.1, 0)
+        stop_timer()
+        rospy.sleep(1)
+        state = 3
+    # wait for now
+    if state == 3:
+        move_robot(-0.1, 0)
 
 
 ##Image callback
 def image_callback(msg):
-
+    global vert_angle
+    global horizontal_line
     # Convert your ROS Image message to OpenCV2
     cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+    #do a threshold based on the grayscale image
+    ret, mask = cv2.threshold(cv2_img, 220, 255, cv2.THRESH_BINARY)
+    #convert to grayscale
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    #cut off the top half of mask
+    new_mask = mask[500:, ::]
+    new_cv2_img = cv2_img[500:, ::]
 
-    # Get the current slider values
-    sat = cv2.getTrackbarPos('Saturation', 'camera_image.jpeg') / 100
-    mask = cv2.getTrackbarPos('Masking', 'camera_image.jpeg')
+    scan_row = 200
+    #find average location of white pixels in one row
+    thresh = np.where(new_mask[scan_row, :] > 80, 1, 0)
+    #draw line along the row we are scanning 
+    cv2.line(new_cv2_img, (0, scan_row), (1280, scan_row), (0, 0, 255), 2)
+    #find position weighted average location of white pixels in one row
+    if(thresh.sum() == 0):
+        location = 0
+    else:
+        location = (thresh * np.arange(1280)).sum() / thresh.sum()
+    #draw circle at the location of the white pixels
+    cv2.circle(new_cv2_img, (int(location), scan_row), 5, (0, 255, 0), -1)
+    # find distance from center of image
 
-    # Convert the image to HSV and adjust the saturation
-    hsv = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2HSV)
-    hsv[..., 1] = hsv[..., 1] * sat
-    img_sat = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    # detect horizontal lines
+    lines = cv2.HoughLinesP(new_mask, 1, np.pi/180, 100, minLineLength=100, maxLineGap=10)
+    vertical_lines = lines
 
-    # Apply a mask to the image
-    mask_img = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2GRAY)
-    _, mask_img = cv2.threshold(mask_img, mask, 255, cv2.THRESH_BINARY)
-    img_masked = cv2.bitwise_and(img_sat, img_sat, mask=mask_img)
+    # filter only lines with slope between -0.1 and 0.1
+    if lines is not None:
+        lines = [line for line in lines if abs((line[0][3] - line[0][1]) / (line[0][2] - line[0][0])) > -0.1]
+        lines = [line for line in lines if abs((line[0][3] - line[0][1]) / (line[0][2] - line[0][0])) < 0.1]
+    
+    # check if there enough horizontal lines to be considered a horizontal line
+    if lines is not None:
+        if len(lines) > 5:
+            horizontal_line = True
+        else:
+            horizontal_line = False
 
-    # Display image in a window called "camera_image.jpeg"
-    cv2.imshow('win2', cv2_img)
+    # filter only lines from vertical lines with slope greater than 5 or less than -5 
+    if vertical_lines is not None:
+        vertical_lines = [line for line in vertical_lines if abs((line[0][3] - line[0][1]) / (line[0][2] - line[0][0])) > 0.3]
+    
+    # average all the vertical lines
+    if vertical_lines is not None:
+        # take each line and subtract x1 from x2 and y1 from y2
+        vertical_lines_x2 = [(line[0][2] - line[0][0]) for line in vertical_lines]
+        vertical_lines_y2 = [(line[0][3] - line[0][1]) for line in vertical_lines]
+        # take the average of the x2 and y2
+        average_x2 = np.average(vertical_lines_x2)
+        average_y2 = np.average(vertical_lines_y2)
+        # construct a line with the average x2 and y2
+        average_line = np.array([[0, 0, average_x2, average_y2]])
+    else:
+        average_line = None
 
-    #wait key
+    # draw lines on image
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(new_cv2_img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+
+    # draw vertical lines on image
+    if vertical_lines is not None:
+        for line in vertical_lines:
+            x1, y1, x2, y2 = line[0]
+            cv2.line(new_cv2_img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    
+    # draw average line on image
+    if average_line is not None:
+        #check if average is nan
+        if np.isnan(average_line).any():
+            average_line = np.array([[0, 0, 0, 0]])
+        x1, y1, x2, y2 = average_line[0]
+        #cast to int
+        x1 = int(x1)
+        y1 = int(y1)
+        x2 = int(x2)
+        y2 = int(y2)
+        cv2.line(new_cv2_img, (640, 300), (y2 + 640, x2 + 300), (255, 255, 0), 2)
+        vert_angle = y2
+
+    # Display image in a window called "win3"d
+    cv2.imshow('win3', new_mask)
+    cv2.imshow('win2', new_cv2_img)
+
     cv2.waitKey(1)
 
 ## Move Robot node
 # use ros to publish to the topic /cmd_vel:
-def move_robot(x):
+def move_robot(x, z):
     twist = Twist()
     twist.linear.x = x
-    twist.angular.z = 0.0
+    twist.angular.z = z
     pub.publish(twist)
     rate.sleep()
 
@@ -66,15 +157,11 @@ rospy.init_node('topic_publisher', anonymous=True)
 pub = rospy.Publisher('/R1/cmd_vel', Twist, queue_size=1)
 sub = rospy.Subscriber('/R1/pi_camera/image_raw', Image, image_callback)
 pub_plate = rospy.Publisher('/license_plate', String, queue_size=1)
-rate = rospy.Rate(2) # 10hz
+rate = rospy.Rate(10) # 10hz
 #wait 1 second
 rospy.sleep(1)
 
 while not rospy.is_shutdown():
-    start_timer()
-    move_robot(1)
-    rospy.sleep(1)
-    move_robot(0)
-    stop_timer()
-    #sleep forever
-    rospy.spin()
+    print('state: ', state)
+    rospy.sleep(0.1)
+    state_machine()
